8fbe5430aab9ab40f7ea075739af3feed88ebb34
Milind Shakya <milind.shakya@dealertrack.com>
2015-06-14 09:40:10 -0400

    Updated settings to reflect correct redirect url

--- faceguard/settings.py
+++ faceguard/settings.py
@@ -31,7 +31,7 @@ ACCESS_TOKEN_URL = 'https://graph.facebook.com/v2.3/oauth/access_token?'
 CLIENT_ID = '177316008955964'
 CLIENT_APP_SECRET = 'a98cec4142c3cd8ed35800b48696f136'
 GRANT_TYPE = 'client_credentials'
-REDIRECT_URL = 'http://dev.ahfctoolkit.com.ngrok.com/facebook_login_success'
+REDIRECT_URL = 'http://faceguard.herokuapp.com/facebook_login_success'
 SENDER_EMAIL = 'milind.sakya@gmail.com'
 # Application definition
 
