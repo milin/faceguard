6a45516410e395af9cb0f1cff18a41cbbd18f9ef
Milind Shakya <milind.shakya@dealertrack.com>
2015-06-14 20:03:09 -0400

    Update sendgrid username

--- faceguard/settings.py
+++ faceguard/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE_CLASSES = (
 )
 import os
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'sendgrid_username'
+EMAIL_HOST_USER = 'app37812569@heroku.com'
 EMAIL_HOST_PASSWORD = 'sendgrid_password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
