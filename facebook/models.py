66a932b0c0c185ea9162bffa1e8be132d46ad7a4
Milind Shakya <milind.shakya@dealertrack.com>
2015-06-16 11:42:23 -0400

    Added maxlength for char

--- facebook/models.py
+++ facebook/models.py
@@ -10,7 +10,7 @@ class FacebookUser(models.Model):
     username=models.CharField(max_length=50)
     user=models.ForeignKey(User)
     blacklist_words = models.TextField(blank=True)
-    password = models.CharField(null=True, blank=True)
+    password = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.first_name
